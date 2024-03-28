b8214d6d0cce82499d2f4851f17f38d0980c190d
swapno963 <swapno963@gmail.com>
2024-03-28 15:27:24 +0600

    Hero section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { Navbar } from "./components";
+import { Navbar, Hero } from "./components";
 // import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
-          {/* <Hero /> */}
+          <Hero />
         </div>
         {/* <About />
         <Experience />
