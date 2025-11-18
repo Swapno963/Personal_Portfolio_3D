d53727da977333b195ca8ae479abf90c06455a0a
swapno963 <swapno963@gmail.com>
2025-11-18 23:32:39 +0600

    made hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 `}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -18,8 +18,8 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Swapno</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Backend Software Engineer with deep Django/FastAPI expertise <br className='sm:block hidden' /> and familiar with frontend Technologies(React js, Next js).
+          <p className={`${styles.heroSubText} mt-3 mb-2 text-white-100`}>
+            Backend Software Engineer with deep Django/FastAPI <br className='sm:block hidden' /> and familiar with frontend Technologies(React js, Next js).
           </p>
         </div>
       </div>
